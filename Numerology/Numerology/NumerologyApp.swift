4643ccb451451d76e3accb1d927da83e404243b5
kirio-1234 <ioribear1228@gmail.com>
2025-01-06 12:12:27 +0900

    Xcodeプロジェクトを追加

---
+++ Numerology/Numerology/NumerologyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumerologyApp.swift
+//  Numerology
+//
+//  Created by Iori Suzuki on 2025/01/06.
+//
+
+import SwiftUI
+
+@main
+struct NumerologyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
